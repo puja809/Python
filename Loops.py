ae65d5ee2ebfcb4252a67ee349e423a44c4bcdaf
Puja <pujachatterjee809@gmail.com>
2022-05-07 10:04:11 +0530

    For Loop in Python

---
+++ Loops.py
@@ -0,0 +1,16 @@
+"""For Loop in Python"""
+
+# Iterating a list:
+list=[["Puja","Engineer"],["Dora","Doctor"],["Diya","Biology Master"]]
+for items in list:
+    print(items)
+
+# Iterating the keys in dictionary:
+dict1={
+    "Name":"Puja",
+    "Address":"Bardhaman",
+    "Job":"Engineer"
+}
+
+for items in dict1:
+    print(items)
