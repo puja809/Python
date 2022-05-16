d2b772f206d52fd6e1435335b4b22052e83d5367
Puja <pujachatterjee809@gmail.com>
2022-05-16 10:52:55 +0530

    Lambda Function in Python

---
+++ Lambda.py
@@ -0,0 +1,14 @@
+"""
+       Lambda Functions are also known as Anonymous functions
+"""
+
+# Lambda is a one liner function.
+minus = lambda x, y: x - y
+print(minus(3, 1))
+
+
+def minus(x, y):
+    return x-y
+
+
+print(minus(80, 40))
