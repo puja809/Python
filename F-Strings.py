09d3b97d6a510b10ce3b7c1881bc56fd3b62eb70
Puja <pujachatterjee809@gmail.com>
2022-05-17 09:37:00 +0530

    F-String

---
+++ F-Strings.py
@@ -0,0 +1,11 @@
+"""
+                                    F-Strings
+
+Help in fast string formatting (adding various string, numeric values and
+special characters to a string)
+"""
+
+import math
+str1 = "Puja"
+str = f"I am {str1} {30*13+89} {math.pow(2,3)}"
+print(str)
