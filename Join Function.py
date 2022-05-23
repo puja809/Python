b84b412ffa8d9d88f25992039bd41f740a64fdc4
Puja <pujachatterjee809@gmail.com>
2022-05-23 10:09:57 +0530

    Join Function in Python

---
+++ Join Function.py
@@ -0,0 +1,10 @@
+"""
+                                 Join Function in Python
+"""
+
+lst = ["Apple", "Mango", "Litchi", "Papaya", "Watermelon"]
+
+# Joining the above list element with coma using join function
+
+a = ",".join(lst)
+print(a)
