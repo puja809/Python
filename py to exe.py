f94a4491dfafc8c6f6825a111f4d8687689fa9f7
Puja <pujachatterjee809@gmail.com>
2022-06-08 08:27:59 +0530

    Converting .py to .exe steps in Python

---
+++ py to exe.py
@@ -0,0 +1,8 @@
+"""
+Steps to convert .py file to .exe file
+    1. install pyinstaller (pip install pyinstaller --user)
+    2. convert the .py or any other extension with python code to .exe (pyinstaller <filename> )
+    3. two folder will be generated build & dist
+    4. dist will have the corresponding .exe file
+    5. To generate only one file (pyinstaller  --onefile <filename>)
+"""
