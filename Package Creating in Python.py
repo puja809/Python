1a044b8d6d19f832dec3227dabe94eaf7c032739
Puja <pujachatterjee809@gmail.com>
2022-06-09 09:59:57 +0530

    Customized Package creating and installation in Python

---
+++ Package Creating in Python.py
@@ -0,0 +1,13 @@
+"""
+                                            Creating Package
+
+Steps:
+    1. create a .py file for package
+    2. Create a setup file
+    3. pip install wheel --user
+    4. python setup.py sdist bdist_wheel
+    5. enter the dist folder created (cd dist)
+    6. pip install package001-1.0.0-py3-none-any.whl --user (Install the wheel file for the package)
+    7. Now we can import our customized package and use its functions and classes anywhere in our code
+
+"""
