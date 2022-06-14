8ee3224f0fc41196f650b327048d1b65bcfb33db
Puja <pujachatterjee809@gmail.com>
2022-06-14 11:03:27 +0530

    py to exe in Python

--- py to exe.py
+++ py to exe.py
@@ -5,4 +5,6 @@ Steps to convert .py file to .exe file
     3. two folder will be generated build & dist
     4. dist will have the corresponding .exe file
     5. To generate only one file (pyinstaller  --onefile <filename>)
+    Note: to avoid closing of the .exe terminal add a (a = input()) at the end of the code.
+    This will make the code wait for an input from user hence it will not close instantly
 """
